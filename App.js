c0bd1a4da3580dd943ef7906a01b170aadd120e3
griselda4774 <trandinhtam4774@gmail.com>
2023-05-22 09:45:09 +0700

    Add some Modal

--- App.js
+++ App.js
@@ -13,23 +13,29 @@ import ChooseCategoriesModal from "./src/components/ChooseCategoriesModal";
 import EditCategoriesModal from "./src/components/EditCategoryModal";
 import EditTaskScreen from "./src/components/EditTaskScreen";
 import EditTaskTitleModal from "./src/components/EditTaskTitleModal";
-import { BLACK_BACKGROUND_COLOR } from "./src/constants/Constants";
+import { BLACK_BACKGROUND_COLOR, GREY_COLOR } from "./src/constants/Constants";
 import DeleteTaskModal from "./src/components/DeleteTaskModal";
 import EditPriorityModal from "./src/components/EditPriorityModal";
 import ChoosePriorityModal from "./src/components/ChooseTaskPriority";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import DatePicker from "react-native-modern-datepicker";
 
 const App = () => {
+  const date = new Date();
   return (
     <SafeAreaView style={styles.container}>
-      <EditTaskTitleModal />
-      <StatusBar style="light" />
+      <DatePicker mode="calendar" />
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: BLACK_BACKGROUND_COLOR,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: GREY_COLOR,
   },
 });
 
