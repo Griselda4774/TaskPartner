3abbbd9bf12acb9bc94d96eea87a7049417b615d
ngphth1312 <ngphth1312@gmail.com>
2023-05-30 16:52:30 +0700

    small commit to save code before testing merge

--- src/components/tempData.js
+++ src/components/tempData.js
@@ -1,5 +1,36 @@
+
+
 export default tempData = [
-    {
-        
-    }
+  {
+    title: "Do Math Homework",
+    description: "Description 01",
+    isDone: false,
+    dueTime: "30/05/2023",
+    dueDate: "16:45",
+    category: "#809CFF",
+    icon: "../../assets/googleIcon.png",
+    priority: 1,
+  },
+
+  {
+    title: "Tack out dogs",
+    description: "Description 02",
+    isDone: false,
+    dueTime: "22/05/2023",
+    dueDate: "12:34",
+    category: "##FF8080",
+    icon: "../../assets/googleIcon.png",
+    priority: 2,
+  },
+
+  {
+    title: "Do Math Homework",
+    description: "Description 03",
+    isDone: false,
+    dueTime: "13/12/2023",
+    dueDate: "22:13",
+    category: "##FFCC80",
+    icon: "../../assets/googleIcon.png",
+    priority: 3,
+  },
 ];
